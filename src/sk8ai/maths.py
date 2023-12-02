37a50562ae9c54cb48abab936777a3d57a057a81
arcvelit <louka.f.s@gmail.com>
2023-12-02 13:12:47 -0500

    Derivatives update

--- src/sk8ai/maths.py
+++ src/sk8ai/maths.py
@@ -1,22 +1,22 @@
-from numpy import tanh, maximum, log, exp
+import numpy as np
 
 # Sigmoid activation and rel. derivative
 def sigmoid():
-	return lambda x: 1./(1.+ exp(-x)), lambda x: x* (1.-x)
+	return lambda x: 1./(1.+ np.exp(-x)), lambda x: x* (1.-x)
 
 # Hyperbolic tangent activation and rel. derivative
 def tanh():
-	return lambda x: tanh(x), lambda x: 1.-tanh(x)**2
+	return lambda x: np.tanh(x), lambda x: 1.-x**2
 
 # ReLU activation and derivative
 def ReLU():
-	return lambda x: maximum(0,x), lambda x: x>0
+	return lambda x: np.maximum(0,x), lambda x: x>0
 
 # Softplus or Smooth ReLU activation and rel. derivative
 def softplus():
-	return lambda x: log(1.+exp(x)), lambda x: 1./(1.+exp(-x))
+	return lambda x: np.log(1.+np.exp(x)), lambda x: 1.-np.exp(-x)
 
 # Linear activation and derivative
 def linear():
-	return lambda x: x, lambda x: 1
+	return lambda x: x, lambda x: 1.
 
